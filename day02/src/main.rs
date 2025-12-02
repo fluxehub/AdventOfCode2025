8f0af13ca70d9d6bf965c938233d32f485937e58
fluxehub <53355311+fluxehub@users.noreply.github.com>
2025-12-02 10:30:39 +0000

    day 2 solution

---
+++ day02/src/main.rs
@@ -0,0 +1,43 @@
+use aoc::*;
+
+#[parse]
+fn get_ids(input: &str) -> Result<Vec<u64>> {
+    input
+        .split(",")
+        .map(|s| {
+            let (start, end) = s.split_once("-").ok_or_eyre("Missing separator")?;
+            let start = start.trim().parse()?;
+            let end = end.trim().parse()?;
+            Ok(start..=end)
+        })
+        .flatten_ok()
+        .collect::<Result<Vec<_>>>()
+}
+
+#[part_one]
+fn count_duplicated_patterns(ids: &[u64]) -> u64 {
+    ids.iter()
+        .filter(|id| {
+            let id_string = id.to_string(); // Have to bind or we get temp value dropped :(
+            let (first, second) = id_string.split_at(id_string.len() / 2);
+            first == second
+        })
+        .sum()
+}
+
+#[part_two]
+fn count_repeating_patterns(ids: &[u64]) -> u64 {
+    ids.iter()
+        .filter(|id| {
+            let id_string = id.to_string();
+            for pos in 1..=(id_string.len() / 2) {
+                if id_string.as_bytes().chunks(pos).all_equal() {
+                    return true;
+                }
+            }
+            false
+        })
+        .sum()
+}
+
+aoc_day!(2);
